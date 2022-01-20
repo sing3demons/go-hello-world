d9513f62867fa41d1ba450cca886eb7f8785930c
sing3demons <sing3demons@live.com>
2022-01-20 18:45:49 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/monitor"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+)
+
+var (
+	buildcommit = "dev"
+	buildtime   = time.Now().String()
+)
+
+func main() {
+	_, err := os.Create("/tmp/live")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove("/tmp/live")
+
+	app := fiber.New()
+	app.Use(recover.New())
+
+	app.Get("/dashboard", monitor.New())
+	app.Use(logger.New(logger.ConfigDefault))
+	app.Get("/x", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"buildcommit": buildcommit,
+			"buildtime":   buildtime,
+		})
+	})
+	// Readiness Probe
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON("hello, world!")
+	})
+	app.Get("/healthz", func(c *fiber.Ctx) error { return c.SendStatus(fiber.StatusOK) })
+
+	//Graceful Shutdown
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	go func() {
+		if err := app.Listen(":8080"); err != nil {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	<-ctx.Done()
+	stop()
+
+	fmt.Println("shutting down gracefully, press Ctrl+C again to force")
+
+	if err := app.Shutdown(); err != nil {
+		fmt.Println(err)
+	}
+}
